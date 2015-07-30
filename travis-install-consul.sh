6a5a2d067e91f7ab6cb2273c7f82a62580d691d9
Gavin M. Roy <gavinr@aweber.com>
2015-07-29 22:02:46 -0400

    Travis build updates

--- travis-install-consul.sh
+++ travis-install-consul.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CONSUL_VERSION=${CONSUL_VERSION:-0.5.0}
+CONSUL_VERSION=${CONSUL_VERSION:-0.5.2}
 if [ ! -d "${HOME}/consul" ]; then
   echo "Downloading Consul ${CONSUL_VERSION}";
   curl -s -L -o /tmp/${CONSUL_VERSION}_linux_amd64.zip https://dl.bintray.com/mitchellh/consul/${CONSUL_VERSION}_linux_amd64.zip
--- travis-umbrella-setup.sh
+++ travis-umbrella-setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 BUILD_DIR=${BUILD_DIR:-${HOME}/rabbitmq-public-umbrella}
-RABBIT_TAG=${RABBIT_TAG:-v3_5_1}
+RABBIT_TAG=${RABBIT_TAG:-v3_5_4}
 echo "Build dir: ${BUILD_DIR}"
 echo "Travis build dir: ${TRAVIS_BUILD_DIR}"
-if [ ! -d $BUILD_DIR ]; then
-    git clone https://github.com/rabbitmq/rabbitmq-public-umbrella.git $BUILD_DIR
-    cd $BUILD_DIR
-    make co
+if [ -d ${BUILD_DIR} ]; then
+    rm -rf ${BUILD_DIR}
 fi
-cd $BUILD_DIR
-make BRANCH=rabbitmq_${RABBIT_TAG} up_c
-rm -rf ${BUILD_DIR}/rabbitmq-autocluster-consul
+git clone https://github.com/rabbitmq/rabbitmq-public-umbrella.git ${BUILD_DIR}
+cd ${BUILD_DIR}
+git checkout -b rabbitmq_${RABBIT_TAG}
+git pull origin rabbitmq_${RABBIT_TAG}
+make co
 cp -r ${TRAVIS_BUILD_DIR} ${BUILD_DIR}/
