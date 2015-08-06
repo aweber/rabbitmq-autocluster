b0ecf3eb893f5b911f7dea84e34dd883c100db4b
Gavin M. Roy <gavinr@aweber.com>
2015-08-06 00:14:31 -0400

    Add additional config options

--- include/autocluster.hrl
+++ include/autocluster.hrl
@@ -8,6 +8,7 @@
 
 -define(CONFIG_MAP,
         [{config, backend,              "AUTOCLUSTER_TYPE",      consul,      atom},
+         {config, autocluster_host,     "AUTOCLUSTER_HOST",      "undefined", string},
          {config, longname,             "RABBITMQ_LONGNAME",     false,       atom},
          {config, cluster_name,         "CLUSTER_NAME",          "undefined", string},
          {config, consul_acl,           "CONSUL_ACL",            "undefined", string},
@@ -17,6 +18,11 @@
          {config, consul_service,       "CONSUL_SERVICE",        "rabbitmq",  string},
          {config, consul_service_port,  "CONSUL_SERVICE_PORT",   5672,        integer},
          {config, consul_service_prefix,"CONSUL_SERVICE_PREFIX", undefined,   string},
-         {config, consul_service_ttl,   "CONSUL_SERVICE_TTL",    30,          integer}]).
+         {config, consul_service_ttl,   "CONSUL_SERVICE_TTL",    30,          integer},
+         {config, etcd_scheme,          "ETCD_SCHEME",           "http",      string},
+         {config, etcd_host,            "ETCD_HOST",             "localhost", string},
+         {config, etcd_port,            "ETCD_PORT",             2379,        integer},
+         {config, etcd_prefix,          "ETCD_PREFIX",           "rabbitmq",  string},
+         {config, etcd_node_ttl,        "ETC_NODE_TTL",          30,          integer}]).
 
 -define(CONSUL_CHECK_NOTES, list_to_atom("RabbitMQ Auto-Cluster Plugin TTL Check")).
