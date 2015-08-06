402fa5e51b00805dfd876dfbdd6a6fa4cf05ff14
Gavin M. Roy <gavinr@aweber.com>
2015-08-06 13:54:38 -0400

    Update the clustering logic
    - Will now properly not try and rejoin a cluster that it is a member of
    - Will not try and cluster nodes that can't be joined against

--- src/autocluster.erl
+++ src/autocluster.erl
@@ -24,8 +24,10 @@
 init() ->
   application:ensure_started(inets),
   case ensure_registered() of
-    ok    -> ensure_clustered();
-    error -> ok
+    {ok, DiscoveryNodes} -> ensure_clustered(DiscoveryNodes);
+    error ->
+      autocluster_log:info("Error in ensuring clustered"),
+      ok
   end.
 
 
@@ -34,15 +36,25 @@ init() ->
 %% @doc Check to see if the node is already defined in a cluster, joining if required
 %% @end
 %%
-ensure_clustered() ->
+ensure_clustered(DiscoveryNodes) ->
+  Discovery = sets:del_element(node(), sets:from_list(DiscoveryNodes)),
   Nodes = rabbit_mnesia:cluster_nodes(all),
-  case Nodes of
-    [] ->
-      autocluster_log:info("Appears to be first node"),
-      ok;
-    _  ->
+  case length(Nodes) of
+    1 ->
+      autocluster_log:debug("Node is only node in the cluster"),
+      DNodes = sets:to_list(Discovery),
+      case DNodes of
+        [] ->
+          autocluster_log:info("Node appears to be the first in the cluster"),
+          ok;
+        _ ->
+          join_cluster(DNodes)
+      end;
+    _ ->
       case lists:member(node(), Nodes) of
-        true  -> ok;
+        true ->
+          autocluster_log:info("Node is already in the cluster"),
+          ok;
         false -> join_cluster(Nodes)
       end
   end.
@@ -84,13 +96,13 @@ ensure_registered(Name, Module) ->
   case Module:nodelist() of
     {ok, Nodes} ->
       case lists:member(node(), Nodes) of
-        true  -> ok;
+        true  -> {ok, Nodes};
         false ->
           autocluster_log:info("Registering node with ~p", [Name]),
           case Module:register() of
             ok ->
               autocluster_log:debug("Registered node"),
-              ok;
+              {ok, Nodes};
             {error, Reason} ->
               autocluster_log:error("Error registering node with ~p: ~p", [Name, Reason]),
               error
@@ -108,7 +120,7 @@ ensure_registered(Name, Module) ->
 %% @end
 %%
 filter_dead_nodes(Nodes) ->
-  lists:filter(fun(N) -> net_adm:ping(list_to_atom(N)) =:= ping end, Nodes).
+  lists:filter(fun(N) -> net_adm:ping(N) =:= pong end, Nodes).
 
 
 %% @private
@@ -117,13 +129,20 @@ filter_dead_nodes(Nodes) ->
 %% @end
 %%
 join_cluster(Nodes) ->
-  Alive = filter_dead_nodes(Nodes),
-  autocluster_log:debug("Joining existing cluster: ~p", [Alive]),
-  application:stop(rabbit),
-  mnesia:stop(),
-  rabbit_mnesia:reset(),
-  rabbit_mnesia:join_cluster(lists:nth(1, Alive), disc),
-  mnesia:start(),
-  rabbit:start(),
-  autocluster_log:info("Cluster joined"),
-  ok.
+  case filter_dead_nodes(Nodes) of
+    [] ->
+      autocluster_log:warning("Can not communicate with cluster nodes: ~p",
+        [Nodes]),
+      ok;
+    Alive ->
+      autocluster_log:debug("Joining existing cluster: ~p", [Alive]),
+      application:stop(rabbit),
+      mnesia:stop(),
+      rabbit_mnesia:reset(),
+      rabbit_mnesia:join_cluster(lists:nth(1, Alive),
+                                 autocluster_config:get(node_type)),
+      mnesia:start(),
+      rabbit:start(),
+      autocluster_log:debug("Cluster joined"),
+      ok
+  end.
