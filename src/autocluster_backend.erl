4dce988dfebac3c97075ed4d0a1149f9bbd4f1ea
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2015-08-07 11:20:50 +0200

    tuple/2 is an invalid typespec, use {} instead
    The error is reported by Erlang 18.0. Previous versions accepted it.

--- src/autocluster_backend.erl
+++ src/autocluster_backend.erl
@@ -5,8 +5,8 @@
 %%==============================================================================
 -module(autocluster_backend).
 
--callback nodelist() -> tuple('ok', Nodes :: list())|tuple('error', Reason :: string()).
+-callback nodelist() -> {'ok', Nodes :: list()}|{'error', Reason :: string()}.
 
--callback register() -> 'ok'|tuple('error', Reason :: string()).
+-callback register() -> 'ok'|{'error', Reason :: string()}.
 
--callback unregister() -> 'ok'|tuple('error', Reason :: string()).
+-callback unregister() -> 'ok'|{'error', Reason :: string()}.
