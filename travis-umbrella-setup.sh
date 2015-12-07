15004f8aae958fc0f3323f505c0746e88dc1ecd5
Gavin M. Roy <gavinr@aweber.com>
2015-12-07 10:02:53 -0500

    Switch to use the make up && make up_c and bump to RabbitMQ 3.5.6

--- travis-umbrella-setup.sh
+++ travis-umbrella-setup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 BUILD_DIR=${BUILD_DIR:-${HOME}/rabbitmq-public-umbrella}
-RABBIT_TAG=${RABBIT_TAG:-v3_5_4}
+RABBIT_TAG=${RABBIT_TAG:-v3_5_6}
 echo "Build dir: ${BUILD_DIR}"
 echo "Travis build dir: ${TRAVIS_BUILD_DIR}"
 git clone https://github.com/rabbitmq/rabbitmq-public-umbrella.git ${BUILD_DIR}
 cd ${BUILD_DIR}
-git checkout -b rabbitmq_${RABBIT_TAG}
-git pull origin rabbitmq_${RABBIT_TAG}
 make co
+make BRANCH=${RABBIT_TAG} up_c
 if [ -d ${BUILD_DIR}/autocluster ]; then
     rm -rf ${BUILD_DIR}/autocluster
 fi
