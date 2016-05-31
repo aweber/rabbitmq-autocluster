0db8626248a89d26ac58e4862b88358672b70613
Gavin M. Roy <gavinmroy@gmail.com>
2016-05-30 22:42:28 -0400

    Add a few utility methods
    - backend_module/0 turns the backend type configuration value and returns the module to use.
    - node_hostname/0 simply returns the hostname for the current node without the tuple that is returned by inet:gethostname/0.

--- src/autocluster_util.erl
+++ src/autocluster_util.erl
@@ -9,6 +9,8 @@
 -export([as_atom/1,
          as_integer/1,
          as_string/1,
+         backend_module/0,
+         node_hostname/0,
          node_name/1,
          parse_port/1]).
 
@@ -24,9 +26,9 @@
 %% @doc Return the value as a list
 %% @end
 %%
-as_atom(Value) when is_atom(Value) =:= true -> Value;
-as_atom(Value) when is_binary(Value) =:= true -> list_to_atom(binary_to_list(Value));
-as_atom(Value) when is_list(Value) =:= true -> list_to_atom(Value);
+as_atom(Value) when is_atom(Value) -> Value;
+as_atom(Value) when is_binary(Value) -> list_to_atom(binary_to_list(Value));
+as_atom(Value) when is_list(Value) -> list_to_atom(Value);
 as_atom(Value) ->
   autocluster_log:error("Unexpected data type for atom value: ~p~n", [Value]),
   Value.
@@ -38,8 +40,8 @@ as_atom(Value) ->
 %% @end
 %%
 as_integer([]) -> undefined;
-as_integer(Value) when is_list(Value) =:= true -> list_to_integer(Value);
-as_integer(Value) when is_integer(Value) =:= true -> Value;
+as_integer(Value) when is_list(Value) -> list_to_integer(Value);
+as_integer(Value) when is_integer(Value) -> Value;
 as_integer(Value) ->
   autocluster_log:error("Unexpected data type for integer value: ~p~n", [Value]),
   Value.
@@ -50,14 +52,48 @@ as_integer(Value) ->
 %% @doc Return the value as a list
 %% @end
 %%
-as_string([]) -> undefined;
-as_string(Value) when is_atom(Value) =:= true -> atom_to_list(Value);
-as_string(Value) when is_binary(Value) =:= true -> binary_to_list(Value);
-as_string(Value) when is_integer(Value) =:= true -> integer_to_list(Value);
-as_string(Value) when is_list(Value) =:= true -> Value;
+as_string([]) -> "";
+as_string(Value) when is_atom(Value) -> as_string(atom_to_list(Value));
+as_string(Value) when is_binary(Value) -> as_string(binary_to_list(Value));
+as_string(Value) when is_integer(Value) -> as_string(integer_to_list(Value));
+as_string(Value) when is_list(Value) -> lists:flatten(Value);
 as_string(Value) ->
   autocluster_log:error("Unexpected data type for list value: ~p~n", [Value]),
   Value.
+
+
+%%--------------------------------------------------------------------
+%% @doc Return the module to use for node discovery.
+%% @spec backend_module() -> module() | undefined
+%% @end
+%%--------------------------------------------------------------------
+-spec backend_module() -> module() | undefined.
+backend_module() ->
+  backend_module(autocluster_config:get(backend)).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc Return the module to use for node discovery.
+%% @spec backend_module(atom()) -> module() | undefined
+%% @end
+%%--------------------------------------------------------------------
+-spec backend_module(atom()) -> module() | undefined.
+backend_module(aws)          -> autocluster_aws;
+backend_module(consul)       -> autocluster_consul;
+backend_module(dns)          -> autocluster_dns;
+backend_module(etcd)         -> autocluster_etcd;
+backend_module(unconfigured) -> undefined.
+
+
+%%--------------------------------------------------------------------
+%% @doc Return the hostname for the current node (without the tuple)
+%% @spec node_hostname() -> string()
+%% @end
+%%--------------------------------------------------------------------
+-spec node_hostname() -> string().
+node_hostname() ->
+  {ok, Hostname} = inet:gethostname(),
+  Hostname.
 
 
 %% @spec node_name(mixed) -> atom()
