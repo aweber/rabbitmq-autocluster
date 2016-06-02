2d9bc86c2793916c44b075e45ee283d037dcc7e5
Gavin M. Roy <gavinr@aweber.com>
2016-06-02 15:05:02 -0400

    Clean up autocluster_config
    - Cleanup coding style
    - Add specs
    - Ensure 100% code coverage
    - Add two-step checking of environment variables who have RABBITMQ_ prefixed in the key. If the RABBITMQ_ prefixed key fails to find a value, check without the prefix (#59)

--- test/src/autocluster_config_tests.erl
+++ test/src/autocluster_config_tests.erl
@@ -1,59 +1,105 @@
 -module(autocluster_config_tests).
 
--export([reset_config/0]).
-
 -include_lib("eunit/include/eunit.hrl").
 
--include("autocluster.hrl").
 
-reset_config() -> reset_config(?CONFIG_MAP).
-reset_config([]) -> ok;
-reset_config([H|T]) ->
-  application:unset_env(autocluster, H#config.key),
-  os:unsetenv(H#config.os),
-  reset_config(T).
+get_test_() ->
+  {
+    foreach,
+    fun autocluster_testing:on_start/0,
+    fun autocluster_testing:on_finish/1,
+    [
+      {
+        "invalid value",
+        fun() ->
+          ?assertEqual(undefined, autocluster_config:get(invalid_config_key))
+        end
+      }
+    ]
+  }.
 
-config_get_backend_default_test() ->
-  reset_config(),
-  Config = lists:keyfind(backend, #config.key, ?CONFIG_MAP),
-  ?assertEqual(Config#config.default, autocluster_config:get(backend)).
 
-config_get_backend_os_test() ->
-  reset_config(),
-  os:putenv("AUTOCLUSTER_TYPE", "etcd"),
-  ?assertEqual(etcd, autocluster_config:get(backend)).
 
-config_get_backend_app_test() ->
-  reset_config(),
-  application:set_env(autocluster, backend, "srv"),
-  ?assertEqual(srv, autocluster_config:get(backend)).
+app_envvar_test_() ->
+  {
+    foreach,
+    fun autocluster_testing:on_start/0,
+    fun autocluster_testing:on_finish/1,
+    [
+      {
+        "app atom value",
+        fun() ->
+          application:set_env(autocluster, longname, true),
+          ?assertEqual(true, autocluster_config:get(longname))
+        end
+      },
+      {
+        "app integer value",
+        fun() ->
+          application:set_env(autocluster, consul_port, 8502),
+          ?assertEqual(8502, autocluster_config:get(consul_port))
+        end
+      },
+      {
+        "app string value",
+        fun() ->
+          application:set_env(autocluster, consul_svc, "rabbit"),
+          ?assertEqual("rabbit", autocluster_config:get(consul_svc))
+        end
+      },
+      {
+        "app string value when binary",
+        fun() ->
+          application:set_env(autocluster, consul_svc, <<"rabbit">>),
+          ?assertEqual("rabbit", autocluster_config:get(consul_svc))
+        end
+      }
+    ]
+  }.
 
-config_get_app_integer_value_test() ->
-  reset_config(),
-  application:set_env(autocluster, consul_port, 8502),
-  ?assertEqual(8502, autocluster_config:get(consul_port)).
 
-config_get_os_integer_value_test() ->
-  reset_config(),
-  os:putenv("CONSUL_PORT", "8501"),
-  ?assertEqual(8501, autocluster_config:get(consul_port)).
+os_envvar_test_() ->
+  {
+    foreach,
+    fun autocluster_testing:on_start/0,
+    fun autocluster_testing:on_finish/1,
+    [
+      {
+        "os atom value",
+        fun() ->
+          os:putenv("RABBITMQ_USE_LONGNAME", "true"),
+          ?assertEqual(true, autocluster_config:get(longname))
+        end
+      },
+      {
+        "os integer value",
+        fun() ->
+          os:putenv("CONSUL_PORT", "8501"),
+          ?assertEqual(8501, autocluster_config:get(consul_port))
+        end
+      },
+      {
+        "prefixed envvar",
+        fun() ->
+          os:putenv("RABBITMQ_USE_LONGNAME", "true"),
+          os:putenv("USE_LONGNAME", "false"),
+          ?assertEqual(true, autocluster_config:get(longname))
+        end
+      },
+      {
+        "no prefixed envvar",
+        fun() ->
+          os:putenv("USE_LONGNAME", "true"),
+          ?assertEqual(true, autocluster_config:get(longname))
+        end
+      },
+      {
+        "docker changing CONSUL_PORT value",
+        fun() ->
+          os:putenv("CONSUL_PORT", "tcp://172.17.10.3:8501"),
+          ?assertEqual(8501, autocluster_config:get(consul_port))
+        end
+      }
+    ]
+  }.
 
-config_get_app_atom_value_test() ->
-  reset_config(),
-  application:set_env(autocluster, longname, true),
-  ?assertEqual(true, autocluster_config:get(longname)).
-
-config_get_os_atom_value_test() ->
-  reset_config(),
-  os:putenv("RABBITMQ_USE_LONGNAME", "true"),
-  ?assertEqual(true, autocluster_config:get(longname)).
-
-config_docker_screws_with_envvar_test() ->
-  reset_config(),
-  os:putenv("CONSUL_PORT", "tcp://172.17.10.3:8501"),
-  ?assertEqual(8501, autocluster_config:get(consul_port)).
-
-config_get_failure_mode_value_test() ->
-  reset_config(),
-  os:putenv("AUTOCLUSTER_FAILURE", "stop"),
-  ?assertEqual(stop, autocluster_config:get(autocluster_failure)).
