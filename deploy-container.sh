7d1595326b5f38b1e6baa7ef708675c29e34aa4a
Gavin M. Roy <gavinr@aweber.com>
2016-06-03 18:52:04 -0400

    Change build script to be run locally

--- deploy-container.sh
+++ deploy-container.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-docker login -u=gavinmroy -p=${DOCKER_PASSWORD}
-docker build -t aweber/rabbitmq-autocluster:${TRAVIS_TAG} .
-docker tag aweber/rabbitmq-autocluster:${TRAVIS_TAG} aweber/rabbitmq-autocluster:latest
-docker push aweber/rabbitmq-autocluster:${TRAVIS_TAG}
+docker build -t aweber/rabbitmq-autocluster:$1 .
+docker tag aweber/rabbitmq-autocluster:$1 aweber/rabbitmq-autocluster:latest
+docker push aweber/rabbitmq-autocluster:$1
 docker push aweber/rabbitmq-autocluster:latest
