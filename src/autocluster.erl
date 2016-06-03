44a29fef634d78baebc3eec83c49f93e2e82353c
Gavin M. Roy <gavinr@aweber.com>
2016-06-03 11:45:44 -0400

    Code style cleanup, logging setup change

--- src/autocluster.erl
+++ src/autocluster.erl
@@ -17,15 +17,18 @@
 -compile(export_all).
 -endif.
 
--spec init() -> ok | error.
-%% @doc Register the node with Consul and then check to see if there are other
-%%      nodes available that this node can cluster with. If so, stop the rabbit
-%%      and mnesia applications, reset the mnesia database, join the cluster,
-%%      and then start the mnesia and rabbit applications back up again.
+%%--------------------------------------------------------------------
+%% @doc
+%% Register the node with Consul and then check to see if there are
+%% other nodes available that this node can cluster with. If so, stop
+%% the rabbit and mnesia applications, reset the mnesia database, join
+%% the cluster, and then start the mnesia and rabbit applications back
+%% up again.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec init() -> ok | error.
 init() ->
-  autocluster_log:maybe_set_default_log_level(),
+  ensure_logging_configured(),
   application:ensure_all_started(inets),
   case node_is_registered() of
     error ->
@@ -34,49 +37,60 @@ init() ->
       maybe_cluster_node(Registered)
   end.
 
-
 %% Clustering Logic
 
--spec node_is_registered() -> ok | error.
+%%--------------------------------------------------------------------
 %% @private
-%% @doc Step 1: get the backend type from config and pass it to step two.
+%% @doc
+%% Step 1: get the backend type from config and pass it to step two.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec node_is_registered() -> ok | error.
 node_is_registered() ->
   ensure_registered(autocluster_config:get(backend)).
 
 
--spec ensure_registered(atom()) -> ok | error.
+%%--------------------------------------------------------------------
 %% @private
-%% @doc Step 2: get the module for the backend type and pass it to step three.
+%% @doc
+%% Step 2: get the module for the backend type and pass it to step three.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec ensure_registered(atom()) -> ok | error.
 ensure_registered(aws) ->
   autocluster_log:debug("Using AWS backend"),
   ensure_registered(aws, autocluster_aws);
+
 ensure_registered(consul) ->
   autocluster_log:debug("Using consul backend"),
   ensure_registered(consul, autocluster_consul);
+
 ensure_registered(dns) ->
   autocluster_log:debug("Using DNS backend"),
   ensure_registered(dns, autocluster_dns);
+
 ensure_registered(etcd) ->
   autocluster_log:debug("Using etcd backend"),
   ensure_registered(etcd, autocluster_etcd);
+
 ensure_registered(unconfigured) ->
-  autocluster_log:error("Backend is not configured, please read the documentation"),
+  autocluster_log:error("Backend is not configured"),
   error;
+
 ensure_registered(Backend) ->
   autocluster_log:error("Unsupported backend: ~s.", [Backend]),
   error.
 
-
--spec ensure_registered(Name :: atom(), Module :: module()) -> ok | error.
+%%--------------------------------------------------------------------
 %% @private
-%% @doc Step 3: get the list of nodes from the service discovery backend and
-%%      pass them to step four to possibly register the node with the backend.
+%% @doc
+%% Step 3: get the list of nodes from the service discovery backend
+%% and pass them to step four to possibly register the node with the
+%% backend.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec ensure_registered(Name :: atom(), Module :: module())
+    -> ok | error.
 ensure_registered(Name, Module) ->
   maybe_delay_startup(),
   autocluster_log:info("Starting ~p registration.", [Name]),
@@ -84,30 +98,37 @@ ensure_registered(Name, Module) ->
   maybe_register(Nodes, Name, Module).
 
 
--spec maybe_register({ok, [node()]} | {error, atom()},
-                     Name :: atom(),
-                     Module :: module()) -> ok | error.
+%%--------------------------------------------------------------------
 %% @private
-%% @doc Step 4: evaluate the node list to determine if the node should be
-%%      registered, passing the info off to step five if so.
+%% @doc
+%% Step 4: evaluate the node list to determine if the node should be
+%% registered, passing the info off to step five if so.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec maybe_register({ok, [node()]} | {error, atom()},
+  Name :: atom(),
+  Module :: module()) -> ok | error.
 maybe_register({ok, Nodes}, Name, Module) ->
-  maybe_register_non_member_node(lists:member(node(), Nodes), Name, Module, Nodes);
+  maybe_register_non_member_node(lists:member(node(), Nodes),
+                                 Name, Module, Nodes);
 maybe_register({error, Reason}, _, Module) ->
-  autocluster_log:error("Could not fetch node list from ~p: ~p.", [Module, Reason]),
+  autocluster_log:error("Could not fetch node list from ~p: ~p.",
+                        [Module, Reason]),
   error.
 
 
--spec maybe_register_non_member_node(true | false,
-                                     Name :: atom(),
-                                     Module :: module(),
-                                     Nodes :: [node()]) -> ok | error.
+%%--------------------------------------------------------------------
 %% @private
-%% @doc Step 5: If the node is already registered, move on to the next phase,
-%%      if not, register it with the backend passing the response to step six.
+%% @doc
+%% Step 5: If the node is already registered, move on to the next
+%% phase, if not, register it with the backend passing the response
+%% to step six.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec maybe_register_non_member_node(true | false,
+  Name :: atom(),
+  Module :: module(),
+  Nodes :: [node()]) -> ok | error.
 maybe_register_non_member_node(true, _, _, Nodes) ->
   {ok, Nodes};
 maybe_register_non_member_node(false, Name, Module, Nodes) ->
@@ -115,28 +136,35 @@ maybe_register_non_member_node(false, Name, Module, Nodes) ->
   process_registration_result(Module:register(), Name, Nodes).
 
 
--spec process_registration_result(ok | error, Name :: atom(), Nodes :: [node()])
-    -> ok | error.
+%%--------------------------------------------------------------------
 %% @private
-%% @doc Step 6: Evaluate the response returning ok or error.
+%% @doc
+%% Step 6: Evaluate the response returning ok or error.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec process_registration_result(ok | error,
+                                  Name :: atom(),
+                                  Nodes :: [node()])
+    -> ok | error.
 process_registration_result(ok, Name, Nodes) ->
   autocluster_log:debug("Registered node with ~p.", [Name]),
   {ok, Nodes};
 process_registration_result({error, Reason}, Name, _) ->
-  autocluster_log:error("Error registering node with ~p: ~p.", [Name, Reason]),
+  autocluster_log:error("Error registering node with ~p: ~p.",
+                        [Name, Reason]),
   error.
 
 
--spec maybe_cluster_node({ok, [node()] | error}) -> ok | error.
+%%--------------------------------------------------------------------
 %% @private
-%% @doc Phase Two (Step 7): If nodes are returned from service discovery
-%%      check to ensure that the node is part of the RabbitMQ cluster with them.
-%%      If it isn't join the cluster. If the service discovery registration
-%%      check failed, return an error.
+%% @doc
+%% Phase Two (Step 7): If nodes are returned from service discovery
+%% check to ensure that the node is part of the RabbitMQ cluster with
+%% them. If it isn't join the cluster. If the service discovery
+%% registration check failed, return an error.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec maybe_cluster_node({ok, [node()] | error}) -> ok | error.
 maybe_cluster_node({ok, Nodes}) ->
   autocluster_log:debug("Discovered ~p", [Nodes]),
   ensure_clustered(Nodes);
@@ -145,25 +173,30 @@ maybe_cluster_node(error) ->
   startup_failure().
 
 
--spec ensure_clustered([node()]) -> ok|error.
+%%--------------------------------------------------------------------
 %% @private
-%% @doc Step 8: Remove the node from the node list from the service
-%%      discovery backend. Get the list of nodes that this node already knows
-%%      about and potentially join the node to the cluster.
+%% @doc
+%% Step 8: Remove the node from the node list from the service
+%% discovery backend. Get the list of nodes that this node already
+%% knows about and potentially join the node to the cluster.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec ensure_clustered([node()]) -> ok|error.
 ensure_clustered(Nodes) ->
   Others = sets:del_element(node(), sets:from_list(Nodes)),
   maybe_join_cluster_nodes(rabbit_mnesia:cluster_nodes(all), Others).
 
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Step 9: Evaluate the nodes in the RabbitMQ cluster and returned
+%% from the discovery service and determine if the node should join
+%% the cluster.
+%% @end
+%%--------------------------------------------------------------------
 -spec maybe_join_cluster_nodes(RNodes :: [node()], DNodes :: [node()])
     -> ok | error.
-%% @private
-%% @doc Step 9: Evaluate the nodes in the RabbitMQ cluster and returned from
-%%      the discovery service and determine if the node should join the cluster.
-%% @end
-%%
 maybe_join_cluster_nodes(_, []) ->
   autocluster_log:debug("Node appears to be the first in the cluster."),
   ok;
@@ -173,49 +206,63 @@ maybe_join_cluster_nodes(Nodes, _) ->
   maybe_join_existing_cluster(lists:member(node(), Nodes), Nodes).
 
 
--spec maybe_join_discovery_nodes([node()]) -> ok | error.
-%% @doc Step 10: If there are discovery nodes to cluster with, have the node
-%%      join the cluster.
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Step 10: If there are discovery nodes to cluster with, have the
+%% node join the cluster.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec maybe_join_discovery_nodes([node()]) -> ok | error.
 maybe_join_discovery_nodes([]) ->
-  autocluster_log:debug("No other nodes are registed with service discovery."),
+  autocluster_log:debug("No other nodes are registed with the backend."),
   ok;
+
 maybe_join_discovery_nodes(Nodes) ->
   join_cluster(Nodes).
 
 
--spec maybe_join_existing_cluster(true | false, [node()]) -> ok | error.
-%% @doc Step 11: Evaluate the check to see if the node is already in the cluster
-%%      info returned by RabbitMQ and if not, join the cluster.
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Step 11: Evaluate the check to see if the node is already in the
+%% cluster info returned by RabbitMQ and if not, join the cluster.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec maybe_join_existing_cluster(true | false, [node()])
+    -> ok | error.
 maybe_join_existing_cluster(true, _) ->
   autocluster_log:debug("Node is already in the cluster"),
   ok;
+
 maybe_join_existing_cluster(false, Nodes) ->
   join_cluster(Nodes).
 
 
--spec join_cluster([node()]) -> ok | error.
+%%--------------------------------------------------------------------
 %% @private
-%% @doc Step 12: Filter any nodes that this node can not communicate with and
-%%      pass the list on to Step 13.
+%% @doc
+%% Step 12: Filter any nodes that this node can not communicate with
+%% and pass the list on to Step 13.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec join_cluster([node()]) -> ok | error.
 join_cluster(Nodes) ->
   join_cluster_nodes(filter_dead_nodes(Nodes)).
 
 
--spec join_cluster_nodes([node()]) -> ok | error.
+%%--------------------------------------------------------------------
 %% @private
-%% @doc Step 13: Join the node to the existing nodes in the cluster unless there
-%%      are no nodes that it could not commuicate with.
+%% @doc
+%% Step 13: Join the node to the existing nodes in the cluster unless
+%% there are no nodes that it could not communicate with.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec join_cluster_nodes([node()]) -> ok | error.
 join_cluster_nodes([]) ->
   autocluster_log:warning("Can not communicate with cluster nodes."),
   startup_failure();
+
 join_cluster_nodes(Nodes) ->
   autocluster_log:debug("Joining the cluster."),
   application:stop(rabbit),
@@ -229,57 +276,72 @@ join_cluster_nodes(Nodes) ->
   ok.
 
 
--spec filter_dead_nodes([node()]) -> list().
+%%--------------------------------------------------------------------
 %% @private
-%% @doc Filter the list of nodes specified by the backend and ensure that
-%%      this node can communicate with them.
+%% @doc
+%% Filter the list of nodes specified by the backend and ensure that
+%% this node can communicate with them.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec filter_dead_nodes([node()]) -> list().
 filter_dead_nodes(Nodes) ->
   lists:filter(fun(N) -> net_adm:ping(N) =:= pong end, Nodes).
 
+
 %% Startup Failure Methods
 
--spec startup_failure() -> ok | error.
+
+%%--------------------------------------------------------------------
 %% @private
-%% @doc Lookup the configuration value for autocluster failures, returning
-%%      the appropriate value to have the application startup pass or fail.
+%% @doc
+%% Lookup the configuration value for autocluster failures, returning
+%% the appropriate value to have the application startup pass or fail.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec startup_failure() -> ok | error.
 startup_failure() ->
   startup_failure_result(autocluster_config:get(autocluster_failure)).
 
--spec startup_failure_result(atom()) -> ok | error.
+
+%%--------------------------------------------------------------------
 %% @private
-%% @doc Evaluate the configuration value for autocluster failures and
-%%      return the appropriate value to have the application startup pass or
-%%      fail.
+%% @doc
+%% Evaluate the configuration value for autocluster failures and
+%% return the appropriate value to have the application startup pass
+%% or fail.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec startup_failure_result(atom()) -> ok | error.
 startup_failure_result(stop) -> error;
 startup_failure_result(ignore) -> ok;
 startup_failure_result(Value) ->
   autocluster_log:error("Invalid startup failure setting: ~p~n", [Value]),
   ok.
 
+
 %% Startup Delay Methods
 
--spec maybe_delay_startup() -> ok.
+
+%%--------------------------------------------------------------------
 %% @private
-%% @doc Get the configuration for the maximum startup delay in seconds and then
-%%      sleep a random amount.
+%% @doc
+%% Get the configuration for the maximum startup delay in seconds and
+%% then sleep a random amount.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec maybe_delay_startup() -> ok.
 maybe_delay_startup() ->
   startup_delay(autocluster_config:get(startup_delay) * 1000).
 
 
--spec startup_delay(integer()) -> ok.
+%%--------------------------------------------------------------------
 %% @private
-%% @doc Sleep a random number of seconds determined between 0 and the maximum
-%%      value specified.
+%% @doc
+%% Sleep a random number of seconds determined between 0 and the
+%% maximum value specified.
 %% @end
-%%
+%%--------------------------------------------------------------------
+-spec startup_delay(integer()) -> ok.
 startup_delay(0) -> ok;
 startup_delay(Max) ->
   <<A:32, B:32, C:32>> = crypto:rand_bytes(12),
@@ -287,3 +349,19 @@ startup_delay(Max) ->
   Duration = random:uniform(Max),
   autocluster_log:info("Delaying startup for ~pms.", [Duration]),
   timer:sleep(Duration).
+
+
+%% Misc Method(s)
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Configure autocluster logging at the configured level, if it's not
+%% already configured at the same level.
+%% @end
+%%--------------------------------------------------------------------
+-spec ensure_logging_configured() -> ok.
+ensure_logging_configured() ->
+  Level = autocluster_config:get(autocluster_log_level),
+  autocluster_log:set_level(Level).
