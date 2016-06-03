72c4ebc90a2b857739ce26f1f0116306d2aa79b9
Gavin M. Roy <gavinr@aweber.com>
2016-06-03 18:39:05 -0400

    Docker build test

--- deploy-container.sh
+++ deploy-container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker login -u=gavinmroy -p=${DOCKER_PASSWORD}
+docker build -t aweber/rabbitmq-autocluster:${TRAVIS_TAG} .
 docker tag aweber/rabbitmq-autocluster:${TRAVIS_TAG} aweber/rabbitmq-autocluster:latest
 docker push aweber/rabbitmq-autocluster:${TRAVIS_TAG}
 docker push aweber/rabbitmq-autocluster:latest
