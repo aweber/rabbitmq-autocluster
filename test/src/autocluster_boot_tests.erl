f471352365c985fd91e37716869584ec7ef15279
Alexey Lebedeff <alebedev@mirantis.com>
2016-07-06 16:57:13 +0300

    Fixing test runner
    - Test proper commit with TravisCI
    - Make tests passing both with `make tests` and `rebar3 eunit.
    - Use rabbit build system to fetch most of test deps
    - Use covertool plugin for rebar3 - adding rebar3 to the zoo of build
      tools is a bit too much.

--- test/src/autocluster_boot_tests.erl
+++ test/src/autocluster_boot_tests.erl
@@ -9,16 +9,25 @@ process_join_result_test_() ->
            {{error, inconsistent_cluster}, stop, error}],
   autocluster_testing:with_mock_each(
     [{application, [unstick, passthrough]},
-     {mnesia, [unstick, passthrough]},
-     {rabbit_mnesia, passthrough}],
+     {mnesia, unstick},
+     rabbit_mnesia, rabbit, rabbit_log],
     lists:map(fun({JoinResult, FailureSetting, ExpectedResult}) ->
-                     {lists:flatten(io_lib:format("Join returns ~p when failure mode is ~s", [JoinResult, FailureSetting])),
-                      fun () ->
-                          meck:expect(application, stop, fun(_) -> ok end),
-                          meck:expect(mnesia, stop, fun() -> ok end),
-                          meck:expect(rabbit_mnesia, reset, fun() -> ok end),
-                          meck:expect(rabbit_mnesia, join_cluster, fun(_, _) -> JoinResult end),
-                          os:putenv("AUTOCLUSTER_FAILURE", atom_to_list(FailureSetting)),
-                          ?assertEqual(ExpectedResult, autocluster:join_cluster_nodes(['some-fake@node']))
-                      end}
+                      {lists:flatten(io_lib:format("Join returns ~p when failure mode is ~s -> ~p",
+                                                   [JoinResult, FailureSetting, ExpectedResult])),
+                       fun () ->
+                               meck:expect(rabbit_log, log, fun(_, _, _, _) -> ok end),
+                               meck:expect(application, stop, fun(_) -> ok end),
+                               meck:expect(mnesia, stop, fun() -> ok end),
+                               meck:expect(mnesia, start, fun() -> ok end),
+                               meck:expect(rabbit, start, fun() -> ok end),
+                               meck:expect(rabbit_mnesia, reset, fun() -> ok end),
+                               meck:expect(rabbit_mnesia, join_cluster, fun(_, _) -> JoinResult end),
+                               os:putenv("AUTOCLUSTER_FAILURE", atom_to_list(FailureSetting)),
+                               ?assertEqual(ExpectedResult, autocluster:join_cluster_nodes(['some-fake@node'])),
+                               ?assert(meck:validate(rabbit_log)),
+                               ?assert(meck:validate(rabbit_mnesia)),
+                               ?assert(meck:validate(rabbit)),
+                               ?assert(meck:validate(application)),
+                               ?assert(meck:validate(mnesia))
+                       end}
               end, Cases)).
